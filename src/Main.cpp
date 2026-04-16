9a0cfb1cb2172a5cda35cefc86b31ac2d671253c
Ravali <madineni.ravali@zensar.com>
2026-04-16 11:14:00 +0530

    Minor changes a/c to C++17 version

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,41 +5,57 @@
 #include <thread>
 #include <unordered_map>
 #include <memory>
+#include <optional>
 
-using namespace std;
+namespace
+{
+    constexpr int ADD_TRADE = 1;
+    constexpr int START_PROCESSING = 2;
+    constexpr int EXIT_APP = 3;
+    using CommandMap = std::unordered_map<int, std::unique_ptr<Command>>;
+}
 
 // Display menu
 void showMenu()
 {
-    std::cout << "\n=== Trade Processing & Settlement Engine ===\n";
-    std::cout << "1. Add Trade\n";
-    std::cout << "2. Start Processing\n";
-    std::cout << "3. Exit\n";
-    std::cout << "Enter your choice: ";
+    std::cout << "\n=== Trade Processing & Settlement Engine ===\n"
+              << "1. Add Trade\n"
+              << "2. Start Processing\n"
+              << "3. Exit\n"
+              << "Enter your choice: ";
+}
+
+std::optional<int> readChoice()
+{
+    int value{};
+    if (std::cin >> value)
+        return value;
+    return std::nullopt;
 }
 
 int main()
 {
     TradeProcessor processor;
+    CommandMap commands;
 
-    unordered_map<int, unique_ptr<Command>> commands;
+    commands.emplace(ADD_TRADE,
+        std::make_unique<AddTradeCommand>(processor));
 
-    commands.emplace(1, make_unique<AddTradeCommand>(processor));
-    commands.emplace(2, make_unique<StartProcessingCommand>(processor));
-    commands.emplace(3, make_unique<ExitCommand>());
+    commands.emplace(START_PROCESSING,
+        std::make_unique<StartProcessingCommand>(processor));
+
+    commands.emplace(EXIT_APP,
+        std::make_unique<ExitCommand>());
 
     while (true)
     {
         showMenu();
-
-        int choice{};
-        cin >> choice;
-
-        auto it = commands.find(choice);
-        if (it != commands.end()) {
-            it->second->execute();
-        } else {
-            cout << "Invalid choice. Please try again.\n";
+        
+        if (auto choice = readChoice(); choice && commands.count(*choice))
+            commands[*choice]->execute();
+        else
+        {
+            std::cout << "Invalid choice. Please try again.\n";
             break;
         }
     }
